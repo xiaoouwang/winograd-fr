322c932b832f30e6a30cefc2e698b1a4b27e2c67
xiaoou <xiaoouwangfrance@gmail.com>
2021-03-04 17:29:29 +0100

    refresh

---
+++ set.sh
@@ -0,0 +1,12 @@
+make html
+cd build/html
+grep -rl _static . | xargs sed -i -e "s/_static/static/g"
+grep -rl _sources . | xargs sed -i -e "s/_sources/sources/g"
+cp -rf _static static
+cp -rf _sources sources
+cd ..
+cp -rf html ../docs
+cd ..
+git add .
+git commit -m "refresh"
+git push origin master --force
